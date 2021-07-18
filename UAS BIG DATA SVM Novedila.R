33c6094a3cc6239b6b088b852fe5c8549c2d6a91
Frogcodile <87577341+Frogcodile@users.noreply.github.com>
2021-07-18 20:58:33 +0700

    Add files via upload

---
+++ UAS BIG DATA SVM Novedila.R
@@ -0,0 +1,17 @@
+str(UAS_SVM)
+install.packages("e1071")
+library(e1071)
+plot(UAS_SVM$Income...3, UAS_SVM$Income...6, col=UAS_SVM$Pelunasan_Kredit)
+plot(UAS_SVM$Income...3, UAS_SVM$Average_Balance, col=UAS_SVM$Pelunasan_Kredit)
+s<-sample(999,270)
+s
+col<- c("Income...3", "Income...6", "Pelunasan_Kredit")
+UAS_SVM_train <- UAS_SVM[s,col]
+UAS_SVM_test <- UAS_SVM[-s, col]
+svmfit <- svm(Pelunasan_Kredit ~., data = UAS_SVM_train, kernel = "linear", cost = .1, scale = FALSE)
+print(svmfit)
+plot(svmfit, UAS_SVM_train[,col])
+p <- predict(svmfit, UAS_SVM_test[,col], type="class")
+plot(p)
+table(p)
+mean(p== UAS_SVM_test[,3])
